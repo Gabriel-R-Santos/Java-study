81d1bce66e79f715019ce627544029035f69507a
biel-rds <gabriel.rds.developer@gmail.com>
2021-04-12 09:25:40 -0300

    Commit app RentRoom

---
+++ study/src/entities/Room.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Room {
+	
+	private String name;
+	private String email;
+	
+	public Room(String name, String email) {		
+		this.name = name;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	
+}
